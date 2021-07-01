52d40250ca8e66f2faeaf8341367e48f0d9ba79d
Cesar Cisneros <cesarcisneros9@gmail.com>
2021-06-30 20:40:37 -0500

    music app functional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import ButtonAppBar from './Components/AppBar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Login from './Components/Login';
+import Dashboard from './Components/Dashboard'
+
+class App extends Component {
+  state = {
+    loggedIn : false
+  };
+
+  setLogin = (value) => {
+    this.setState({loggedIn : value});
+  }
+
+  render() {
+    const { loggedIn } = this.state;
+
+    return (
+      <div className="App">
+        <ButtonAppBar />
+        {
+          loggedIn
+          ? <Dashboard />
+          : <Login onLogin={this.setLogin} />
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
